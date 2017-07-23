17e8921255d7be08496b03275d17a9b071127447
Georgi Kiryakov <george.kiryakov@gmail.com>
2017-07-23 16:46:47 +0300

    add category endpoint

--- api/server/graph/resolvers/query.js
+++ api/server/graph/resolvers/query.js
@@ -5,6 +5,9 @@ const Query = {
   categories() {
     return Category.find().populate('places');
   },
+  category(root, { id }) {
+  return Category.findById(id).populate('places');
+  }
 };
 
 export default Query;
